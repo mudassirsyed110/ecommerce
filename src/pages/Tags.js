d12ab0fbf1b9ae78f15d09944104dee54aa0e1d0
praburam@cyb.co.uk <mudassir@nimapinfotech.com>
2021-08-25 10:13:15 +0530

    style changes

--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -7,6 +7,7 @@ import { onGetTags } from "../redux/Tags/tagAction";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    paddingRight: "3rem",
   },
   paper: {
     padding: theme.spacing(2),
